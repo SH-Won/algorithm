5a19294475ec7c1b279ea7b2978d59e6e095f155
SH-Won <1317good@naver.com>
2021-08-01 14:00:50 +0900

    bfs

--- 문제/No_2206_2.js
+++ 문제/No_2206_2.js
@@ -9,19 +9,30 @@
 //     '0111',
 //     '0000'
 // ]
+// const input =[
+//     '4 4',
+//     '0111',
+//     '1111',
+//     '1111',
+//     '1110'
+// ]
 const input =[
-    '4 4',
+    '6 4',
+    '0000',
+    '1110',
+    '1000',
+    '0000',
     '0111',
-    '1111',
-    '1111',
-    '1110'
+    '0000'
 ]
+
 const [N,M]=input[0].split(' ').map(num => +num);
 const load = Array.from({length:N},(_,i)=>input[i+1].split('').map(num=>+num));
 let visited = Array.from({length:N},()=>Array.from({length:M},()=>Array(2).fill(0)));
 const distance = [[1,0],[-1,0],[0,1],[0,-1]]
 let way =bfs(0,0);
 console.log(way);
+console.log(visited)
 
 
 function bfs(y,x){
@@ -34,7 +45,7 @@ function bfs(y,x){
 
         if(curY ===N-1 && curX ===M-1){
             
-            return visited[N][M][block];
+            return visited[curY][curX][block];
         }
         
         
@@ -48,7 +59,7 @@ function bfs(y,x){
                 visited[nextY][nextX][block-1] = visited[curY][curX][block] +1;
                 queue.push([nextY,nextX,block-1]);
             }
-            if(!load[nextY][nextX] && !visited[nextY][nextX][0] && !visited[nextY][nextX][1]){
+            if(!load[nextY][nextX] && !visited[nextY][nextX][block] ){
                 visited[nextY][nextX][block] = visited[curY][curX][block] +1;
                 queue.push([nextY,nextX,block])
 
---
+++ 문제/No_7562.js
@@ -0,0 +1,47 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input =[
+    '3','8','0 0','7 0','100','0 0','30 50','10','1 1','1 1'
+]
+
+let inputIndex = 0;
+let T = +input[inputIndex++];
+
+while(T--){
+    const L = +input[inputIndex++];
+    let chessBoard = Array.from({length:L},()=>Array(L).fill(0));
+    const [startY,startX] = input[inputIndex++].split(' ').map(num => +num);
+    const [endY,endX] = input[inputIndex++].split(' ').map(num => +num);
+    const distance = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]];
+
+    bfs(startY,startX);
+
+    function bfs(startY,startX){
+        let t = 0;
+        let queue = [[startY,startX,t]]
+        chessBoard[startY][startX] = 1;
+
+        while(queue.length){
+            const [curY,curX,curT] =queue.shift();
+
+            if(curY === endY && curX ===endX){
+                console.log(curT);
+                break;
+            }
+            
+            distance.forEach(([y,x])=>{
+                const [nextY,nextX,nextT] = [curY+y,curX+x,curT+1];
+                if(nextY < 0 || nextX <0 || nextY >=L || nextX>=L){
+                    return;
+                }
+                if(!chessBoard[nextY][nextX]){
+                    chessBoard[nextY][nextX] = 1;
+                    queue.push([nextY,nextX,nextT])
+                }
+            })
+           
+
+        }
+    }
+
+}
