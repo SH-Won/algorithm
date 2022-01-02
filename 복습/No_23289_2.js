4d77b9e48a9e1cf3a292334688d66c5a0c3dbd05
SH-Won <1317good@naver.com>
2022-01-02 14:55:07 +0900

    remind

---
+++ 복습/No_23289_2.js
@@ -0,0 +1,263 @@
+// const input =[
+//     '7 8 1',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 0 0 4 0 0 0',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 5 0 0 0 0 0',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 0 0 3 0 0 0',
+//     '3',
+//     '5 4 0',
+//     '4 4 1',
+//     '5 6 0'
+// ]
+
+// const input = [
+// '7 8 1',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 4 0 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 5 5 0 0 0 0',
+// '0 0 0 0 0 5 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 3 0 0 0',
+// '3',
+// '4 4 1',
+// '5 4 0',
+// '5 6 0',
+// ]
+// const input = [
+// '7 8 5',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 4 0 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 5 5 0 0 0 0',
+// '0 0 0 0 0 5 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 3 0 0 0',
+// '3',
+// '4 4 1',
+// '5 4 0',
+// '5 6 0',
+// ]
+// const input = [
+// '7 8 7',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 4 0 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 5 5 0 0 0 0',
+// '0 0 0 0 0 5 0 0',
+// '0 0 0 0 0 0 0 0',
+// '0 0 0 0 3 0 0 0',
+// '3',
+// '4 4 1',
+// '5 4 0',
+// '5 6 0',
+// ]
+// const input = [
+//     '7 8 70',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 0 0 4 0 0 0',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 5 5 0 0 0 0',
+//     '0 0 0 0 0 5 0 0',
+//     '0 0 0 0 0 0 0 0',
+//     '0 0 0 0 3 0 0 0',
+//     '3',
+//     '4 4 1',
+//     '5 4 0',
+//     '5 6 0',
+//     ]
+    // const input = [
+    // '7 8 1000',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 0 0 4 0 0 0',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 5 5 0 0 0 0',
+    // '0 0 0 0 0 5 0 0',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 0 0 3 0 0 0',
+    // '3',
+    // '4 4 1',
+    // '5 4 0',
+    // '5 6 0'
+    // ]
+    // const input = [
+    // '7 8 100',
+    // '0 0 0 0 0 0 0 0',
+    // '5 0 0 0 4 0 0 0',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 5 5 0 0 0 0',
+    // '0 0 0 0 0 5 0 0',
+    // '5 0 0 0 0 0 5 0',
+    // '0 0 0 0 3 0 0 0',
+    // '0'
+    // ]
+    // const input = [
+    // '7 8 100',
+    // '0 0 0 0 0 0 5 0',
+    // '5 4 4 4 4 4 4 0',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 5 5 0 0 0 0',
+    // '0 0 0 0 0 5 0 0',
+    // '5 0 0 0 0 0 5 0',
+    // '0 0 0 0 3 0 0 0',
+    // '3',
+    // '4 4 1',
+    // '5 4 0',
+    // '5 6 0',
+    // ]
+    // const input = [
+    // '7 8 1000',
+    // '0 0 0 0 0 0 5 0',
+    // '5 4 4 4 4 4 4 0',
+    // '0 0 0 0 0 0 0 0',
+    // '0 0 5 5 0 0 0 0',
+    // '0 0 0 0 0 5 0 0',
+    // '5 0 0 0 0 0 5 0',
+    // '0 0 0 0 3 0 0 0',
+    // '3',
+    // '4 4 1',
+    // '5 4 0',
+    // '5 6 0',
+    // ]
+    // const input = [
+    // '7 8 1000',
+    // '0 0 0 0 0 0 0 0',
+    // '4 4 4 4 4 4 4 4',
+    // '0 0 0 0 0 5 0 0',
+    // '0 0 5 5 0 0 5 0',
+    // '0 0 0 0 0 5 0 0',
+    // '5 0 0 0 0 0 5 0',
+    // '3 3 3 3 3 3 3 3',
+    // '3',
+    // '4 4 1',
+    // '5 4 0',
+    // '5 6 0',
+    // ]
+    // const input =[
+    //     '7 8 1',
+    //     '0 0 0 0 0 0 0 0',
+    //     '0 0 0 0 0 0 0 0',
+    //     '1 0 0 0 0 0 0 0',
+    //     '0 0 5 0 0 0 0 0',
+    //     '0 0 0 0 0 0 0 0',
+    //     '0 0 0 0 0 0 0 0',
+    //     '0 0 0 0 0 0 0 0',
+    //     '1',
+    //     '3 4 1',
+        
+    // ]
+//const fs = require('fs');
+//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [R,C,K] = input[0].split(' ').map(Number);
+const map = Array.from({length:R},(_,i)=>input[i+1].split(' ').map(Number));
+const W = +input[R+1];
+const walls = Array.from({length:W},(_,i)=>input[i+R+2].split(' ').map(Number));
+const [dy,dx] = [[0,0,-1,1],[1,-1,0,0]];
+const [hy,hx] = [[[-1,1],[-1,1],[0,0],[0,0]],[[0,0],[0,0],[1,-1],[1,-1]]];
+
+const isValidPos = (y,x) => (y>=0 && x>=0 && y<R && x<C);
+const warmHouse = (warmer,map,wall) =>{
+    let visited = Array.from({length:R},()=>Array(C).fill(false));
+    const [y,x,d] = warmer;
+    let queue =[[y+dy[d],x+dx[d],5]];
+    map[y+dy[d]][x+dx[d]] += 5;
+    while(queue.length){
+        const [y,x,k] = queue.shift();
+        if(k===1) return;
+        const [ny,nx] = [y+dy[d],x+dx[d]];
+        const [ly,lx] = [ny+hy[d][0],nx+hx[d][0]];
+        const [ry,rx] = [ny+hy[d][1],nx+hx[d][1]];
+        if(isValidPos(ny,nx) && !visited[ny][nx] && !wall.has(`${y},${x},${ny},${nx}`)){
+            visited[ny][nx] = true;
+            queue.push([ny,nx,k-1]);
+            map[ny][nx]+=k-1;
+        }
+        if(isValidPos(ly,lx) && !visited[ly][lx]){
+            const [py,px] = [ly-dy[d],lx-dx[d]];
+            if(!wall.has(`${y},${x},${py},${px}`) && !wall.has(`${py},${px},${ly},${lx}`)){
+                visited[ly][lx] = true;
+                queue.push([ly,lx,k-1]);
+                map[ly][lx] +=k-1; 
+            }
+        }
+        if(isValidPos(ry,rx) && !visited[ry][rx]){
+            const [py,px] = [ry-dy[d],rx-dx[d]];
+            if(!wall.has(`${y},${x},${py},${px}`) && !wall.has(`${py},${px},${ry},${rx}`)){
+                visited[ry][rx] = true;
+                queue.push([ry,rx,k-1]);
+                map[ry][rx] +=k-1; 
+            }
+        }
+    }
+}
+const controlTemper = (map,wall) =>{
+    let newMap = Array.from({length:R},()=>Array(C).fill(0));
+    for(let y=0; y<R; y++){
+        for(let x=0; x<C; x++){
+            const temper = map[y][x];
+            if(!temper) continue;
+            let totalDecrease = 0;
+            for(let i=0; i<4; i++){
+                const [ny,nx] = [y+dy[i],x+dx[i]];
+                if(!isValidPos(ny,nx) || temper <= map[ny][nx] || wall.has(`${y},${x},${ny},${nx}`)) continue;
+                const t = Math.floor((temper - map[ny][nx]) / 4) 
+                newMap[ny][nx] += t ;
+                totalDecrease += t;
+            }
+            newMap[y][x] += (temper - totalDecrease);
+        }
+    }
+    return newMap;
+}
+const decreaseEdgeTemper = (map) =>{
+    for(let x=0; x<C-1; x++){
+        if(map[0][x]) map[0][x]--;
+    }
+    for(let y=0; y<R-1; y++){
+        if(map[y][C-1]) map[y][C-1]--;
+    }
+    for(let x=C-1; x>0; x--){
+        if(map[R-1][x]) map[R-1][x]--;
+    }
+    for(let y=R-1; y>0; y--){
+        if(map[y][0]) map[y][0]--;
+    }
+}
+const solution = (map,walls)=>{
+    let temper = [];
+    let warmer = [];
+    let wall = new Set();
+    for(let y=0; y<R; y++){
+        for(let x=0; x<C; x++){
+            if(map[y][x]){
+                if(map[y][x] === 5) temper.push([y,x]);
+                else warmer.push([y,x,map[y][x]-1]);
+                map[y][x] = 0;
+            }
+        }
+    }
+    for(let i=0; i<walls.length; i++){
+        const [y,x,t] = walls[i];
+        if(t === 0){
+            wall.add(`${y-1},${x-1},${y-2},${x-1}`);
+            wall.add(`${y-2},${x-1},${y-1},${x-1}`);
+        }
+        else if( t===1){
+            wall.add(`${y-1},${x-1},${y-1},${x}`);
+            wall.add(`${y-1},${x},${y-1},${x-1}`);
+        }
+    }
+    let chocolate = 0;
+    while(chocolate < 100){
+        warmer.forEach(w => warmHouse(w,map,wall));
+        map = controlTemper(map,wall);
+        decreaseEdgeTemper(map);
+        chocolate++;
+        if(!temper.some(([y,x]) => map[y][x] < K)) return console.log(chocolate);
+    }
+    console.log(101);
+}
+solution(map,walls);
