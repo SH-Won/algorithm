c78c286a24c078fb397fc8f3a4eed7d63b7adaf3
SH-Won <1317good@naver.com>
2022-01-06 17:57:51 +0900

    stack & remind

--- 문제/No_9935.js
+++ 문제/No_9935.js
@@ -1,49 +1,32 @@
 //const fs = require('fs');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // const input = ['mirkovC4nizCC44','C4'];
-const input = ['12ab112ab2ab','12ab']
+// const input = ['12ab112ab2ab','12ab']
 
 const solution = (input) =>{
-    let string = input[0];
-    const splitWord = input[1];
-    while(true){
-       const index = string.search(splitWord);
-       if(index === -1) return console.log(string ==='' ? 'FRULA' : string);
-       string = string.substring(0,index) + string.substring(index+splitWord.length);
+    const string = input[0];
+    const exWord = input[1];
+    const len = exWord.length; 
+    let stack = [];
+    for(let i=0; i<string.length; i++){
+        if(string[i] === exWord[len-1]){
+            let flag = true;
+            for(let j=1; j<len; j++){
+                if(stack[stack.length-j] === exWord[len-j-1]) continue;
+                else{
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                let count = len-1;
+                while(count--) stack.pop();
+            }
+            else stack.push(string[i]);
+        }
+        else stack.push(string[i]);
     }
+    const answer = stack.join('');
+    console.log(answer === '' ? 'FRULA' : answer);
 }
 solution(input);
-
-
-
-// let str = input[0].trim();
-// let explode_str = input[1].trim();
-// let len = explode_str.length;
-// let stack = [];
-// for (let i = 0; i < str.length; i++) {
-//   let flag = false;
-//   if (str[i] === explode_str[len - 1]) {
-//     for (let j = 0; j < len - 1; j++) {
-//       if (stack[stack.length - (j + 1)] === explode_str[len - (j + 2)]) {
-//         continue;
-//       }
-//       flag = true;
-//       break;
-//     }
-//     if (flag) {
-//       stack.push(str[i]);
-//     } else {
-//       for (let k = 0; k < len - 1; k++) {
-//         stack.pop();
-//       }
-//     }
-//   } else {
-//     stack.push(str[i]);
-//   }
-// }
-// let result = stack.join("");
-// if (result === "") {
-//   console.log("FRULA");
-// } else {
-//   console.log(stack.join(""));
-// }
---
+++ 복습/No_17822_2.js
@@ -0,0 +1,196 @@
+// const input = ['4 4 1','1 1 2 3','5 2 4 2','3 1 3 5','2 1 3 2','2 0 1'];
+// const input = ['4 4 2','1 1 2 3','5 2 4 2','3 1 3 5','2 1 3 2','2 0 1','3 1 3'];
+// const input = ['4 4 3','1 1 2 3','5 2 4 2','3 1 3 5','2 1 3 2','2 0 1','3 1 3','2 0 2'];
+// const input = ['4 4 4','1 1 2 3','5 2 4 2','3 1 3 5','2 1 3 2','2 0 1','3 1 3','2 0 2','3 1 1'];
+// const input = ['4 6 3','1 2 3 4 5 6','2 3 4 5 6 7','3 4 5 6 7 8','4 5 6 7 8 9','2 1 4','3 0 1','2 1 2']
+// const input = [
+// '5 5 3',
+// '5 1 2 3 5',
+// '5 2 2 4 5',
+// '5 6 2 5 5',
+// '5 5 2 5 2',
+// '2 2 1 2 3',
+// '3 0 2',
+// '4 1 6',
+// '2 0 2',
+// ] //ans 19
+// const input = [
+// '6 5 2',
+// '1 2 3 2 1',
+// '4 5 4 2 3',
+// '6 8 2 1 2',
+// '7 6 2 8 8',
+// '3 2 4 2 8',
+// '5 8 2 1 1',
+// '2 0 2',
+// '2 1 4',
+// ] //ans 55
+// const input = [
+// '4 4 1',
+// '2 1 2 2',
+// '3 2 1 3',
+// '5 4 6 5',
+// '3 2 2 4',
+// '2 0 1'
+// ] //ans 19
+// const input = [
+// '3 3 3',
+// '2 2 1',
+// '1 1 2',
+// '2 2 2',
+// '2 0 1',
+// '2 1 5',
+// '3 0 2',
+// ] //ans 0
+// const input = [
+// '5 5 1',
+// '1 2 1 2 2',
+// '2 1 2 1 1',
+// '1 2 2 1 2',
+// '2 1 1 2 1',
+// '1 3 3 1 1',
+// '3 0 1'
+// ] //ans 4
+// const input = [
+// '6 6 2',
+// '1 1 3 1 1 1',
+// '2 2 4 2 2 2',
+// '4 2 4 2 4 2',
+// '3 1 3 1 3 1',
+// '4 3 4 3 4 5',
+// '2 3 2 3 3 2',
+// '2 1 2',
+// '3 0 4',
+// ] //ans 42
+// const input = [
+// '40 20 8',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 4 1 1 1 1 1 1 1 1 1',
+// '1 1 1 9 5 8 1 1 1 1 1 4 5 4 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '4 5 4 1 4 5 4 1 1 1 1 1 1 5 1 1 1 4 5 4',
+// '1 1 1 1 1 1 2 1 1 1 1 1 4 4 4 1 1 1 1 1',
+// '1 1 4 5 4 1 1 4 5 4 1 1 4 4 4 1 1 4 5 4',
+// '4 5 4 1 1 1 1 1 1 4 5 4 1 1 4 4 4 1 1 1',
+// '1 1 1 1 1 1 4 2 4 1 1 1 1 1 1 1 4 4 4 1',
+// '4 5 4 1 1 1 2 6 5 1 1 4 2 4 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 4 2 4 2',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 4 4 4 1 1 1 1 1',
+// '1 1 1 1 4 5 1 1 1 1 1 1 1 1 4 4 4 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 4 4 4 1',
+// '1 1 1 1 1 1 1 6 2 6 2 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 1 1 1 1 1',
+// '1 1 4 3 4 1 1 1 1 1 4 3 4 1 1 4 3 4 1 1',
+// '1 1 1 1 1 2 1 2 4 2 1 1 1 1 4 2 1 1 1 1',
+// '1 4 3 4 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2',
+// '1 1 1 1 1 4 1 4 1 1 4 1 1 1 1 2 2 2 2 2',
+// '1 1 1 1 1 1 1 1 1 4 1 1 1 1 1 2 2 2 2 2',
+// '2 1 4 2 1 1 1 1 5 2 5 2 5 1 1 1 1 1 1 1',
+// '1 1 1 1 1 6 2 3 1 1 1 1 1 1 1 1 5 2 5 2',
+// '1 1 1 1 1 1 1 1 1 1 6 5 6 5 1 1 1 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 5 4 4 4 3 6 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 3 5 4 4 4 5 1 1',
+// '1 1 1 7 8 7 1 1 1 1 1 1 1 1 1 5 4 4 4 1',
+// '1 1 1 4 5 4 1 1 1 4 5 4 1 1 5 4 5 1 1 1',
+// '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1',
+// '1 1 7 5 6 1 7 6 5 1 7 6 5 1 1 1 1 2 1 1',
+// '1 7 6 7 1 1 1 7 6 5 1 1 1 1 2 7 6 5 1 1',
+// '7 6 5 1 1 6 6 2 4 1 7 6 5 1 1 1 5 1 1 1',
+// '1 1 2 3 2 3 1 1 1 1 1 1 2 5 2 6 1 1 1 1',
+// '1 1 1 1 1 1 2 3 2 3 1 1 1 1 1 2 4 2 4 2',
+// '5 1 3',
+// '6 0 5',
+// '4 1 7',
+// '5 1 3',
+// '3 0 8',
+// '4 1 2',
+// '5 0 1',
+// '3 0 4',
+// ] // ans 478
+
+//const fs = require('fs');
+//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const rotate = (roulette,d,k) =>{
+    const CLOCKWISE = 0;
+    const length = roulette.length;
+    if(d === CLOCKWISE){
+        roulette.unshift(...roulette.splice(length-k))
+    }
+    else {
+        roulette.push(...roulette.splice(0,k));
+    }
+}
+const addNumber = (roulettes) =>{
+     const [sum,count] = roulettes.reduce((acc,roulette) =>{
+       roulette.forEach(num => num && ( acc[0]+=num , acc[1]++));
+       return acc;
+     },Array(2).fill(0))
+     
+     const average = sum / count ;
+     for(let y=0; y<roulettes.length; y++){
+         for(let x=0; x<roulettes[0].length; x++){
+             if(roulettes[y][x]){
+                 const number = roulettes[y][x];
+                 if(number === average) continue;
+                 roulettes[y][x] = number > average ? number-1 : number+1;
+             }
+         }
+     }
+}
+
+const removeNumber = (roulettes,N,M) =>{
+    const [dy,dx] = [[1,-1,0,0],[0,0,1,-1]];
+    const isValidPos = (y,x) => (y>=0 && x>=0 && y<N && x<M);
+    const findNumber = (y,x,number) =>{
+        let count = 0;
+        let queue = [[y,x]];
+        roulettes[y][x] = 0;
+        while(queue.length){
+            const [y,x] = queue.shift();
+            count++;
+            for(let i=0; i<4; i++){
+                const [ny,nx] = [y+dy[i], (x+dx[i]+M) % M];
+                if(!isValidPos(ny,nx) || roulettes[ny][nx] !==number) continue;
+                roulettes[ny][nx] = 0;
+                queue.push([ny,nx]);
+            }
+        }
+        if(count === 1){
+            roulettes[y][x] =number;
+            return false;
+        }
+        return true;
+    }
+    let isRemove = false;
+    for(let y=0; y<N; y++){
+        for(let x=0; x<M; x++){
+            if(roulettes[y][x]){
+                if(findNumber(y,x,roulettes[y][x]) && !isRemove) isRemove = true;
+            }
+        }
+    }
+    if(isRemove) return;
+    return addNumber(roulettes)
+}
+const solution = (input) =>{
+    const [N,M,T] = input[0].split(' ').map(Number);
+    let roulettes = Array.from({length:N},(_,i)=> input[i+1].split(' ').map(Number));
+    const command = Array.from({length:T},(_,i)=>input[i+N+1].split(' ').map(Number));
+    //0 시계방향 1 반시계방향
+    let count = T , i =0;
+    while(count--){
+        const [x,d,k] = command[i++];
+        roulettes.forEach((roulette,index) => (index+1) % x === 0 && rotate(roulette,d,k));
+        removeNumber(roulettes,N,M);
+    }
+    const answer = roulettes.reduce((acc,cur)=> acc+=cur.reduce((acc,cur)=>acc+=cur,0),0);
+    console.log(answer);
+}
+solution(input);
