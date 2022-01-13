fadbb957160372c45ab962aefe9f2c4dabc3db40
SH-Won <1317good@naver.com>
2022-01-13 14:12:05 +0900

    edit

--- 복습/No_6087.js
+++ 복습/No_6087.js
@@ -16,7 +16,7 @@
 // const input = ['4 7','....','..*.','C.*.','..*C','..*.','..*.','....'] //ans2;
 // const input = ['4 7','....','.*..','.*.C','C*..','.*..','.*..','....']; //ans2
 // const input = ['7 4','...C...','.......','.*****.','...C...'] //ans 2
-const input = ['4 6','.C..','....','....','**.*','....','...C'] //ans2
+// const input = ['4 6','.C..','....','....','**.*','....','...C'] //ans2
 // const fs = require('fs');
 // const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
@@ -24,27 +24,30 @@ const getMinMirrorCount = (start,map) =>{
     const [H,W] = [map.length, map[0].length];
     const [dy,dx] =[[1,-1,0,0],[0,0,1,-1]];
     const isValidPos = (y,x) => (y>=0 && x>=0 && y<H && x<W);
-    let install = Array.from({length:H},()=>Array(W).fill(Infinity));
+    let install = Array.from({length:H},()=>Array.from({length:W},()=>Array(4).fill(Infinity)));
     const [y,x] = start;
-    install[y][x] = 0;
     let queue = [] , idx = 0, min = Infinity;
     for(let i=0; i<4; i++){
         const [ny,nx] = [y+dy[i],x+dx[i]];
         if(!isValidPos(ny,nx) || map[ny][nx] === '*') continue;
         queue.push([ny,nx,i]);
-        install[ny][nx] = 0;
+        install[ny][nx][i] = 0;
+        install[y][x][i] = 0;
     }
     while(idx < queue.length){
         const [y,x,dir] = queue[idx++];
-        if(map[y][x] === 'C') min = Math.min(min,install[y][x]);
+        if(map[y][x] === 'C'){
+            min = Math.min(min,install[y][x][dir]);
+            continue;
+        }
         for(let i=0; i<4; i++){
             const [ny,nx] = [y+dy[i],x+dx[i]];
             if(!isValidPos(ny,nx) || map[ny][nx] ==='*') continue;
-            let installCount = install[y][x];
+            let installCount = install[y][x][dir];
             if(dir !==i) installCount++;
-            if(install[ny][nx] >= installCount){
+            if(install[ny][nx][i] > installCount){
                 queue.push([ny,nx,i]);
-                install[ny][nx] = installCount;
+                install[ny][nx][i] = installCount;
             }
         }
     }
