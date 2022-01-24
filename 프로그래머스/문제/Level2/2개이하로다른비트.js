cc9cf715c6e5b60820ab62eff4d9922243eadb8e
SH-Won <1317good@naver.com>
2022-01-25 01:48:58 +0900

    ps

--- 프로그래머스/문제/Level2/2개이하로다른비트.js
+++ 프로그래머스/문제/Level2/2개이하로다른비트.js
@@ -1,26 +1,45 @@
+// const solution = numbers =>{
+//     for(let i=0; i<numbers.length; i++){
+//         const num = numbers[i];
+//         const limit = Math.log2(numbers[i]) >> 0 ;
+//         let j = 0;
+//         for(j=0; j<=limit; j++){
+//            if(!(num & (1<<j))) break;
+//         }
+//         if(j === limit+1) numbers[i] = (1<<j)|(1<<limit ^ num) 
+//         else{
+//             let count = 1;
+//             let temp = num + (1<<j);
+//             for(let k=j-1; k>=0; k--){
+//                 if(count === 2) break;
+//                 if((num & (1<<k))){
+//                     count++;
+//                     temp -= (1<<k);
+//                 }
+//             }
+//             numbers[i] = temp;
+//         }
+//     }
+//     return numbers;
+// }
+// 11000011 11000101 , 10011011 
 const solution = numbers =>{
+    let answer = Array(numbers.length);
     for(let i=0; i<numbers.length; i++){
-        const num = numbers[i];
-        const limit = Math.log2(numbers[i]) >> 0 ;
-        let j = 0;
-        for(j=0; j<=limit; j++){
-           if(!(num & (1<<j))) break;
+        const number = numbers[i];
+        if(number % 2 === 0){
+            answer[i] = number+1;
         }
-        if(j === limit+1) numbers[i] = (1<<j)|(1<<limit ^ num) 
         else{
-            let count = 1;
-            let temp = num + (1<<j);
-            for(let k=j-1; k>=0; k--){
-                if(count === 2) break;
-                if((num & (1<<k))){
-                    count++;
-                    temp -= (1<<k);
-                }
-            }
-            numbers[i] = temp;
+            const bit = '0' + number.toString(2);
+            const index = bit.lastIndexOf('0');
+            answer[i] = parseInt(bit.slice(0,index)+'10'+bit.slice(index+2),2)
+            console.log(answer[i])
         }
     }
-    return numbers;
+    return answer;
 }
-console.log(solution([2,10]))
+console.log(solution([0,7]))
 
+console.log(Math.log2(1))
+console.log(10**15 > 2**53-1)
