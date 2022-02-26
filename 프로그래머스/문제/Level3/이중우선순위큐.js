4604e1274a7175dedc36bfdbb8bd8d50756fa1af
SH-Won <1317good@naver.com>
2022-02-26 16:12:04 +0900

    priority Queue

---
+++ 프로그래머스/문제/Level3/이중우선순위큐.js
@@ -0,0 +1,89 @@
+class Heap{
+    constructor(){
+        this.queue =[];
+    }
+    push = (value) =>{
+        this.queue.push(value);
+        this.up();
+    }
+    isEmpty = () => this.queue.length <= 0;
+}
+class MinHeap extends Heap{
+    pop = (number) =>{
+        if(number === -1){
+            const root = this.queue[0];
+            if(this.queue.length <= 0) return undefined;
+            if(this.queue.length === 1) this.queue = [];
+            else{
+                this.queue[0] = this.queue.pop();
+                this.down();
+            }
+            return root;
+        }
+        else{
+            let last = this.queue[this.queue.length-1];
+            if(this.queue.length === 0) return undefined;
+            if(this.queue.length === 1) this.queue = [];
+            else{
+                let temp = this.queue[this.queue.length-2];
+                if(temp > last){
+                    this.queue[this.queue.length-1] = temp;
+                    this.queue[this.queue.length-2] = last;
+                }
+                last = this.queue.pop();
+            }
+            return last;
+        }
+    }
+    up = () =>{
+        let i = this.queue.length - 1;
+        const inserted = this.queue[i];
+        while(i > 0){
+            const parent = Math.floor((i-1)/2);
+            if(this.queue[parent] > inserted){
+                this.queue[i] = this.queue[parent];
+                i = parent;
+            }else break;
+        }
+        this.queue[i] = inserted;
+    }
+    down = () =>{
+        let i = 0;
+        const root = this.queue[i];
+        while((i*2+1) < this.queue.length){
+            const leftChild = i*2 + 1;
+            const rightChild = i*2 + 2;
+            const smallerChild = this.queue[rightChild] && this.queue[rightChild] < this.queue[leftChild]
+            ? rightChild : leftChild;
+            if(this.queue[smallerChild] <= root){
+                this.queue[i] = this.queue[smallerChild];
+                i = smallerChild;
+            }else break;
+        }
+        this.queue[i] = root;
+    }
+}
+const solution = operations =>{
+    const minHeap = new MinHeap();
+    for(let i=0; i<operations.length; i++){
+        const [alphabet,number] = operations[i].split(' ');
+        if(alphabet === 'I') minHeap.push(+number);
+        else{
+           if(minHeap.isEmpty()) continue;
+           minHeap.pop(+number);
+        }
+    }
+    if(minHeap.queue.length === 0) return [0,0];
+    if(minHeap.queue.length === 1){
+        const number = minHeap.pop(-1);
+        return [number,number];
+    }
+    else{
+        const max = minHeap.pop(1);
+        const min = minHeap.pop(-1);
+        return [max,min];
+    }
+}
+// console.log(solution(["I 16","D 1"]))
+// console.log(solution(["I 7","I 5","I -5","D -1"]))
+console.log(solution(['I 6','I 5','I 2','I 3','I 1','D 1','D -1']));
--- 프로그래머스_복습/합승택시요금_3.js
+++ 프로그래머스_복습/합승택시요금_3.js
@@ -1,42 +1,86 @@
-const makeMinCost = (start,costs,edge) =>{
-    let queue = [[start,0]];
-    while(queue.length){
-        const [current,curFar] = queue.shift();
-        for(let i=0; i<edge[current].length; i++){
-            const {to,far} = edge[current][i];
-            const nextCost = curFar+far;
-            if(costs[start][to] > nextCost){
-                costs[start][to] = nextCost;
-                costs[to][start] = nextCost;
-                queue.push([to,nextCost]);
+class PriorityQueue{
+    constructor(){
+        this.queue = [];
+    }
+    push = (to,cost) =>{
+        this.queue.push({to,cost});
+        this.up();
+    }
+    up = ()=>{
+        let i = this.queue.length -1;
+        const inserted = this.queue[i];
+        while(i > 0){
+            const parent = Math.floor((i-1)/2);
+            if(this.queue[parent].cost > inserted.cost){
+                this.queue[i] = this.queue[parent];
+                i = parent;
+            }else break;
+        }
+        this.queue[i] = inserted;
+    }
+    pop = () =>{
+        const root = this.queue[0];
+        if(this.queue.length <= 0) return undefined;
+        if(this.queue.length === 1) this.queue = [];
+        else{
+            this.queue[0] = this.queue.pop();
+            this.down();
+        }
+        return root;
+    }
+    down = () =>{
+        let i = 0;
+        const root = this.queue[i];
+        while((i*2 + 1) < this.queue.length){
+            const leftChild = i*2 +1;
+            const rightChild = i*2 + 2;
+            const smallerChild = this.queue[rightChild] && this.queue[rightChild].cost < this.queue[leftChild].cost
+            ? rightChild : leftChild;
+            if(this.queue[smallerChild].cost <= root.cost){
+                this.queue[i] = this.queue[smallerChild];
+                i = smallerChild;
+            }else break;
+        }
+        this.queue[i] = root;
+    }
+    isEmpty = () => this.queue.length <= 0;
+}
+const dijkstra = (start,edge) =>{
+    const costs = Array(edge.length).fill(Infinity);
+    const pq = new PriorityQueue();
+    costs[start] = 0;
+    pq.push(start,0);
+    while(!pq.isEmpty()){
+        const cur = pq.pop();
+        if(cur === undefined || cur.cost > costs[cur.to]) continue;
+        for(let i=0; i<edge[cur.to].length; i++){
+            const next = edge[cur.to][i];
+            const nc = next.cost + cur.cost;
+            if(costs[next.to] > nc){
+                costs[next.to] = nc;
+                pq.push(next.to, nc);
             }
         }
     }
+    return costs;
 }
 const solution = (n,s,a,b,fares) =>{
-    // n 지점갯수 
-    let costs = Array.from({length:n+1},()=>Array(n+1).fill(Infinity));
-    const edge = fares.reduce((acc,cur)=>{
-        const [from,to,far] = cur;
-        acc[from].push({to,far});
-        acc[to].push({to:from,far});
-        return acc;
-    },Array.from({length:n+1},()=>[]))
+    const edge = Array.from({length:n+1},()=>[]);
+    fares.forEach(([from,to,cost]) =>(
+        edge[from].push({to,cost}),
+        edge[to].push({to:from,cost})
+    ));
+    const sCost = dijkstra(s,edge);
+    const aCost = dijkstra(a,edge);
+    const bCost = dijkstra(b,edge);
+    let minCost = sCost[a] + sCost[b];
     for(let i=1; i<=n; i++){
-        costs[i][i] = 0;
-    }
-    makeMinCost(s,costs,edge);
-    makeMinCost(a,costs,edge);
-    makeMinCost(b,costs,edge);
-    
-    let minCost = costs[s][a] + costs[s][b];
-    for(let i=1; i<=n; i++){
-        const cost = costs[s][i] + costs[i][a] + costs[i][b];
-        minCost = Math.min(cost,minCost);
+        if(i === s) continue;
+        const cost = sCost[i] + aCost[i] + bCost[i];
+        minCost = Math.min(cost , minCost);
     }
     return minCost;
 }
-//const [n,s,a,b,fares] =[6,4,6,2,[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]];
-//const [n,s,a,b,fares] =[7,3,4,1,[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]];
-const [n,s,a,b,fares] =[6,4,5,6,[[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [2,4,8], [4,3,9]]]
-console.log(solution(n,s,a,b,fares));
+// console.log(solution(6,4,6,2,[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
+// console.log(solution(7,3,4,1,[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]))
+console.log(solution(6,4,5,6,[[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [2,4,8], [4,3,9]]))
