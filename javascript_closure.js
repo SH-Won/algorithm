85f8dff467ee7449d6d07faa0a90e673ed518aa2
SH-Won <1317good@naver.com>
2022-04-01 13:50:52 +0900

    javacript scope

--- javascript_closure.js
+++ javascript_closure.js
@@ -10,10 +10,14 @@ function Clock(time){
     }
 }
 const clock = Clock(10);
+const clock1 = Clock(20);
 clock.pass(10);
+clock1.pass(200);
 console.log(clock.getTime());
 clock.pass(20);
 console.log(clock.getTime());
 console.log(clock.currentTime) // undefined;
+console.log(clock1.getTime());
 // 함수가 이미 실행되어 종료되어도, 함수내부의 지역변수에 도달 할 수 있다.
-// 최신상태 유지가 가능하다
+// 최신상태 유지가 가능하다.
+// 각각 의 함수가 실행되면, 각각의 렉시컬 환경을 가지게 된다.
