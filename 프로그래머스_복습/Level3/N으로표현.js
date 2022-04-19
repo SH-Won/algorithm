0c8defa8172ccd1d2f69ba3c328248c330e3b951
SH-Won <1317good@naver.com>
2022-04-19 23:37:06 +0900

    brute force

---
+++ 프로그래머스_복습/Level3/N으로표현.js
@@ -0,0 +1,21 @@
+const solution = (N,number) =>{
+    const setArr = Array.from({length:9},() => new Set());
+    for(let i=1; i<=8; i++){
+        const num = +N.toString().repeat(i);
+        setArr[i].add(num);
+        for(let j=1; j<i; j++){
+            for(const val1 of setArr[j]){
+                for(const val2 of setArr[i-j]){
+                    setArr[i].add(val1+val2);
+                    setArr[i].add(val1-val2);
+                    setArr[i].add(val1*val2);
+                    setArr[i].add(Math.floor(val1/val2));
+                }
+            }
+        }
+        console.log(setArr[i])
+        if(setArr[i].has(number)) return i;
+    }
+    return -1 ;
+}
+console.log(solution(5,12))
