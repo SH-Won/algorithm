85ff0cd8e0331eb1e7e2af5ccee6dc4a2dbc1638
SH-Won <1317good@naver.com>
2022-05-21 16:36:00 +0900

    javascript type

--- javascript_type.js
+++ javascript_type.js
@@ -44,4 +44,51 @@ console.log(matrix[0] === part); // true 출력
 matrix[0] = [10,11,12];
 console.log(part);  // [1,2,3] 출력
 // matrix[0] 과 part는 같은 메모리주소를 가지고 있었지만, matrix[0]을 새롭게 할당했으므로,
-// matrix[0] 과 part 는 서로 다른 메모리 주소를 가지고 있다
+// matrix[0] 과 part 는 서로 다른 메모리 주소를 가지고 있다.
+
+
+// 래퍼 객체
+const string = 'string';
+console.log(string.length); // 6 출력;
+// 래퍼 객체는 String, Number , Boolean 이 있다.
+// 자바스크립트는 string 이라는 변수의 프로퍼티인 length 에 도달하기위해
+// 오토박싱(auto boxing) 을 하여 임시적으로 string 을 new String('string') 으로 만든다.
+// new String('string')으로 만든 임시 객체는
+// { 0:'s' , 1:'t' , 2:'r' , ..... , length: 6} 인데.
+// 여기서 필요한 프로퍼티인 length 에 도달하고 나서 임시 객체는 사라진다.
+// 여기서 중요한 부분은 string 변수의 'string' 의 원시값에 전혀 변동을 주지 않는다는 점이다.
+
+// 문제
+
+const createObj = (obj) =>{
+    obj.gender = 'male';
+    obj = {
+        name: 'jenny',
+        gender: 'female'
+    }
+    return obj;
+}
+
+const person1 = {name:'seong ho', gender:'female'};
+const person2 = createObj(person1);
+
+// 결과는 어떻게 될까?
+// createObj 함수의 인자인 obj 는 person1 과 같은 주소를 가지게되고
+// 같은 주소를 가진 obj 의 프로퍼티를 변경하면 person1 의 프로퍼티도 동시에 바뀐다.
+// 그리고 나서 obj 의 주소를 아예 다른 객체를 바라보는 주소로 바꾸고 person2 에 리턴을 한다.
+// 결과는 다음과같다.
+console.log(person1); // { name: 'seong ho', gender: 'male' }
+console.log(person2); // { name: 'jenny', gender: 'female' }
+
+// 자바스크립트에서 원시타입은 그 값을 그대로 복사하지만,
+// String, Number, null , boolean 값 , undefined , Symbol(ES6) 를 제외한
+// 모든 객체는 참조형태이므로 항상 이 개념을 숙지해서 코드를 적어야한다.
+
+// 정리
+// 자바스크립트의 원시타입을 제외하면 모든 것은 객체이다.
+// 자바스크립트의 String Number boolean 은 원시타입이면서 래퍼객체이다.
+// 원시타입을 다른 변수에 할당할때 그값을 그대로 복사하지만,
+// 객체를 다른변수에 할당할때는 그값을 복사하는것이 아니라 실제 객체를 바라보고 있는 주소를 복사한다.
+
+
+
